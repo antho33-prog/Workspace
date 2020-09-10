330000622629bcfadf7421faf5b6be4bb40b2e19
anto33-prog <antofacebook@live.com>
2020-09-10 17:54:46 -0300

    Entrega 3 casi completa

--- js/product-info.js
+++ js/product-info.js
@@ -1,4 +1,5 @@
 var product = {};
+var productList = {};
 
 function showImagesGallery(array){
 
@@ -18,6 +19,65 @@ function showImagesGallery(array){
         document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
     }
 }
+
+//productos relacionados//
+
+function showRelatedProducts(array){
+    getJSONData(PRODUCTS_URL).then(function(resultObj) { 
+        if (resultObj.status === "ok") {
+            productList = resultObj.data;
+
+            let htmlRelatedProducts = "";
+
+            for (let i = 0; i <array.length; i++ ) {
+                let relatedProductsPosition = array[i];
+                let relatedProducts = productList[relatedProductsPosition];
+            
+                htmlRelatedProducts +=`
+            
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="` + relatedProducts.imgSrc +`">
+                    <div class="card-body">
+                        <h5 class="card-title"> ` + relatedProducts.name +`</h5>
+                        <p class="card-text">` + relatedProducts.description +`</p>
+                        <a href="product-info.html" class="btn btn-primary">Ver producto</a>
+                    </div
+                </div>
+                `
+        }
+
+        document.getElementById("relatedProductContainer").innerHTML = htmlRelatedProducts;
+    };
+
+    });
+};
+
+//funcion comentario//
+
+function showComents(array) {
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++) {
+
+        htmlContentToAppend += `
+        <div class="list-group">
+            <div class="d-flex flex-row comment-row m-t-0">
+                <div class="comment-text w-100">
+                    <h6 class="font-weight-bold">${array[i].user}</h6>
+                    <div id="rating" + ${i}> </div>
+                    <span class="m-b-15 d-block">${array[i].description}</span>
+                <div class="comment-footer">
+                    <span class="text-muted float-right">${array[i].dateTime}</span>
+                </div>
+                </div>
+            </div>
+            <br>
+            `
+
+            document.getElementById("ListaDeComentarios").innerHTML = htmlContentToAppend;
+    }   
+}
+
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -46,6 +106,21 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             //Muestro las imagenes en forma de galería
             showImagesGallery(product.images);
-        }
-    });
+            
+            showRelatedProducts(product.relatedProducts);
+            }
+    
+            });
+
+            //comentarios//
+
+            getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+                if (resultObj.status === "ok") {
+                    comentario = resultObj.data;
+
+                    showComents(comentario);
+                }
+            });
+            
+        
 });
